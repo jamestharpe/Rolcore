67f5fb8b326c367055f379f278c9f2ce7803fded
James Tharpe <jimmy.tharpe@gmail.com>
2015-01-25 08:12:52 -0500

    Improves test naming convention

--- src/Utilla.Web.Tests/StringExtensionsTest.cs
+++ src/Utilla.Web.Tests/StringExtensionsTest.cs
@@ -20,45 +20,39 @@ namespace Utilla.Web.Tests
         /// A test for ContainsHtml
         /// </summary>
         [TestMethod]
-        public void ContainsHtmlPositiveTest()
+        public void ContainsHtml_ReturnsTrueWhenStringContainsHtml()
         {
             string s = "<a>";
             bool actual = s.ContainsHtml();
-            Assert.AreEqual(true, actual);
+            Assert.IsTrue(actual);
 
             s = "<a href='foobar'>";
             actual = s.ContainsHtml();
-            Assert.AreEqual(true, actual);
+            Assert.IsTrue(actual);
 
             s = "<a href=\"foobar\">";
             actual = s.ContainsHtml();
-            Assert.AreEqual(true, actual);
+            Assert.IsTrue(actual);
 
             s = "<a href=\"foobar\">biz baz";
             actual = s.ContainsHtml();
-            Assert.AreEqual(true, actual);
+            Assert.IsTrue(actual);
 
             s = "<a href=\"foobar\">biz baz</a> bang!";
             actual = s.ContainsHtml();
-            Assert.AreEqual(true, actual);
+            Assert.IsTrue(actual);
         }
 
-        /// <summary>
-        ///A test for ToUri
-        ///</summary>
         [TestMethod]
-        public void ToUriTest()
+        public void ToUri_ConvertsStringToUri()
         {
             string expected = "http://www.utilla.com/";
             string actual = expected.ToUri().ToString();
             Assert.AreEqual(expected, actual);
         }
 
-        /// <summary>
-        ///A test for ToUri
-        ///</summary>
         [TestMethod, ExpectedException(typeof(ArgumentException))]
-        public void ToUriEmptyStringTest()
+        public void ToUri_RequiresNonEmptyString()
         {
             string expected = string.Empty;
             string actual = expected.ToUri().ToString();
