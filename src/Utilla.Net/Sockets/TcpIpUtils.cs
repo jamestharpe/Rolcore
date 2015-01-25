a8f5c9d7d2b39426289cf0aa142ee641be2f9a4c
James Tharpe <jimmy.tharpe@gmail.com>
2015-01-25 08:25:21 -0500

    Improves StyleCop compliance

--- src/Utilla.Net/Sockets/TcpIpUtils.cs
+++ src/Utilla.Net/Sockets/TcpIpUtils.cs
@@ -1,27 +1,40 @@
-﻿using System;
-
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="TcpIpUtils.cs" company="Utilla">
+//     Copyright © Utilla Contributors
+// </copyright>
+//-----------------------------------------------------------------------
 namespace Utilla.Net.Sockets
 {
-    public sealed class TcpIpUtils
-    {
-        private TcpIpUtils(){}
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Diagnostics.Contracts;
 
+    /// <summary>
+    /// Utility methods for working with TCP/IP.
+    /// </summary>
+    public static class TcpIpUtils
+    {
         /// <summary>
         /// Converts an IP address to a <see cref="double"/> representation.
         /// </summary>
         /// <param name="ipAddress">Specifies the IP address (in 0.0.0.0 format) to convert.</param>
         /// <returns>A <see cref="double"/> representation of the specified IP address.</returns>
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1305:FieldNamesMustNotUseHungarianNotation", Justification = "Reviewed.")]
         public static double IpStringToDouble(string ipAddress)
         {
-            if (String.IsNullOrEmpty(ipAddress))
-                throw new ArgumentException("ipAddress is null or empty.", "ipAddress");
+            Contract.Requires<ArgumentException>(!string.IsNullOrEmpty(ipAddress), "ipAddress is null or empty");
 
             double result = 0;
             string[] ipAddressParts = ipAddress.Split('.');
             if (ipAddressParts.Length > 1)
+            {
                 for (int i = ipAddressParts.Length - 1; i >= 0; i--)
-                    result += ((int.Parse(ipAddressParts[i]) % 256) * System.Math.Pow(256, (3 - i)));
+                {
+                    var octet = int.Parse(ipAddressParts[i]);
+                    var mod = octet % 256;
+                    result += mod * System.Math.Pow(256, 3 - i);
+                }
+            }
 
             return result;
         }
